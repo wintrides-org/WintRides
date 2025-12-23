dde9c3ed9530de0be57b32c112ca29f1257a0b10
Olohi18 <ojohn@smith.edu>
2025-12-23 12:25:34 -0500

    Push to develop

---
+++ types/request.ts
@@ -0,0 +1,53 @@
+/* 
+This file defines the types and interface for ride requests in the WinRides application.
+The interface is to help with alignment across all platforms on the app
+The types defined here define what major attributes of a request are
+They are to be used for both frontend and backend development
+Later: form validation and API validation will be added
+*/
+
+
+// defines the request types: immediate request, scheduled request, group request
+export type RequestType =
+    | "IMMEDIATE"
+    | "SCHEDULED"
+    | "GROUP"
+
+// defines the request statuses: draft, open, matched, canceled, expired
+export type RequestStatus =
+| "DRAFT"     // user is filling the form
+| "OPEN"      // submitted, waiting for driver
+| "MATCHED"   // driver accepted
+| "CANCELED"  // rider or system canceled
+| "EXPIRED";  // no driver accepted in time
+
+// defines reusable pieces: location
+export type Location = {
+    label: string;   // human-readable name
+    address: string;
+    // set latitude and longitude as optional to allow shipping for MVP without maps
+    latitude?: number;
+    longitude?: number; 
+  };
+
+// defines the main RideRequest interface
+export interface RideRequest { 
+    id: string;
+    riderId: string;
+
+    type: RequestType;
+    status: RequestStatus;
+
+    pickup: Location;
+    dropoff: Location;
+
+    partySize: number;
+
+    // Only for scheduled or group requests
+    pickupAt?: string; // ISO timestamp
+
+    createdAt: string;
+}
+  
+  
+  
